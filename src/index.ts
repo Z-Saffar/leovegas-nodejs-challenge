c83207a39a6653ea4196af816e5a51cfa267c907
Zahra <zahrasaffar67@gmail.com>
2026-03-07 19:15:56 +0100

    add global error handler

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import usersRouter from './routes/users';
 import authRouter from './routes/auth';
+import { formatErrors } from './utils/jsonapi';
 
 const app = express();
 app.use(express.json());
 
 app.use('/auth', authRouter);
 app.use('/users', usersRouter);
+
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  console.error(err);
+  res.status(500).json(formatErrors([{ status: '500', title: 'Internal Server Error', detail: 'An unexpected error occurred' }]));
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
